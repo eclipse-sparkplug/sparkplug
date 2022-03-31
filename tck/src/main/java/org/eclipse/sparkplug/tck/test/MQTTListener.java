c7e58896a9de45dba67bb6e8af473c9afbdb1769
Ian Craggs <ian.craggs.external@hivemq.com>
2022-03-31 20:59:49 +0100

    Remove Tahu from listener and monitor classes, merge results with those from tests

--- tck/src/main/java/org/eclipse/sparkplug/tck/test/MQTTListener.java
+++ tck/src/main/java/org/eclipse/sparkplug/tck/test/MQTTListener.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2014, 2021 Cirrus Link Solutions and others
+ * Copyright (c) 2014, 2022 Cirrus Link Solutions and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
@@ -20,24 +20,23 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttTopic;
-import org.eclipse.tahu.message.SparkplugBPayloadDecoder;
-import org.eclipse.tahu.message.model.SparkplugBPayload;
-import org.eclipse.tahu.message.model.Topic;
-import org.eclipse.tahu.util.TopicUtil;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import org.eclipse.sparkplug.tck.test.common.Utils;
+import org.eclipse.sparkplug.tck.test.common.SparkplugBProto.*;
+import org.eclipse.sparkplug.tck.test.common.SparkplugBProto.Payload.Metric;
 
 import org.eclipse.sparkplug.tck.sparkplug.Sections;
 import org.jboss.test.audit.annotations.SpecAssertion;
 import org.jboss.test.audit.annotations.SpecVersion;
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.Date;
 
 @SpecVersion(
-        spec = "sparkplug",
-        version = "3.0.0-SNAPSHOT")
+		spec = "sparkplug",
+		version = "3.0.0-SNAPSHOT")
 public class MQTTListener implements MqttCallbackExtended {
 
 	// Configuration
@@ -47,57 +46,63 @@ public class MQTTListener implements MqttCallbackExtended {
 	private String password = "changeme";
 	private String log_topic_name = "SPARKPLUG_TCK/LOG";
 	private MqttTopic log_topic = null;
-	private MqttClient client;
-	
+	private MqttClient client = null;
+
 	private String primary_host_application_id = null;
-	
-    private HashMap testResults = new HashMap<String, String>();
-    String[] testIds = {
-    	"intro-secondary-host-state",
-    	"intro-group-id-string",
-    	"intro-group-id-chars",
-    	"intro-edge-node-id-string",
-    	"intro-edge-node-id-chars",
-    	"intro-device-id-string",
-    	"intro-device-id-chars",    	
-    	"topic-structure-namespace-device-id-associated-message-types",
-    	"topic-structure-namespace-device-id-non-associated-message-types",
-    	"topic-structure-namespace-valid-group-id",
-    	"topic-structure-namespace-valid-edge-node-id",
-    	"topic-structure-namespace-valid-device-id",
-    };
-	
+
+	private HashMap testResults = new HashMap<String, String>();
+	String[] testIds = { "intro-secondary-host-state", "intro-group-id-string", "intro-group-id-chars",
+			"intro-edge-node-id-string", "intro-edge-node-id-chars", "intro-device-id-string", "intro-device-id-chars",
+			"topic-structure", "topic-structure-namespace-device-id-associated-message-types",
+			"topic-structure-namespace-device-id-non-associated-message-types",
+			"topic-structure-namespace-valid-group-id", "topic-structure-namespace-valid-edge-node-id",
+			"topic-structure-namespace-valid-device-id", "payloads_timestamp_in_UTC" };
+
 	public void log(String message) {
 		try {
-			MqttMessage mqttmessage = new MqttMessage(("MQTTListener: "+message).getBytes());
+			MqttMessage mqttmessage = new MqttMessage(("MQTTListener: " + message).getBytes());
 			log_topic.publish(mqttmessage);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		MQTTListener listener = new MQTTListener();
 		listener.run(args);
 	}
-	
+
+	public void clearResults() {
+		for (int i = 0; i < testIds.length; ++i) {
+			testResults.put(testIds[i], "PASS");
+		}
+	}
+
+	public HashMap<String, String> getResults() {
+		HashMap labelledResults = new HashMap<String, String>();
+		for (int i = 0; i < testIds.length; ++i) {
+			labelledResults.put("MQTTListener:" + testIds[i], testResults.get(testIds[i]));
+		}
+		return labelledResults;
+	}
+
 	public void run(String[] args) {
+		if (client != null) {
+			return;
+		}
+
 		System.out.println("*** Sparkplug TCK MQTT Listener ***");
+
+		/*if (args.length < 1) {
+			System.out.println("Parameter must be: primary_host_application_id");
+			return;
+		}
 		
-        if (args.length < 1) {
-        	System.out.println("Parameter must be: primary_host_application_id");
-        	return;
-        }
-        
-        primary_host_application_id = args[0];
-        System.out.println("Primary host application id is "+primary_host_application_id);
-        
-        testResults = new HashMap<String, String>();
-        
-        for (int i = 0; i < testIds.length; ++i) {
-            testResults.put(testIds[i], "");
-        }
-		
+		primary_host_application_id = args[0];
+		System.out.println("Primary host application id is " + primary_host_application_id);*/
+
+		clearResults();
+
 		try {
 			// Connect to the MQTT Server
 			MqttConnectOptions options = new MqttConnectOptions();
@@ -105,26 +110,27 @@ public class MQTTListener implements MqttCallbackExtended {
 			options.setCleanSession(true);
 			options.setConnectionTimeout(30);
 			options.setKeepAliveInterval(30);
-			//options.setUserName(username);
-			//options.setPassword(password.toCharArray());
+			// options.setUserName(username);
+			// options.setPassword(password.toCharArray());
 			client = new MqttClient(serverUrl, clientId);
-			client.setTimeToWait(5000);						// short timeout on failure to connect
+			client.setTimeToWait(5000); // short timeout on failure to connect
 			client.setCallback(this);
 			log_topic = client.getTopic(log_topic_name);
 			client.connect(options);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void connectComplete(boolean reconnect, String serverURI) {
 		System.out.println("Connected!");
-		
+
 		try {
 			// Just listen to all DDATA messages on spBv1.0 topics and wait for inbound messages
-			client.subscribe(new String[]{"spAv1.0/#", "spBv1.0/#", "STATE/#", log_topic_name}, new int[]{2, 2, 2, 2});
-		} catch(Exception e) {
+			client.subscribe(new String[] { "spAv1.0/#", "spBv1.0/#", "STATE/#", log_topic_name },
+					new int[] { 2, 2, 2, 2 });
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -132,37 +138,44 @@ public class MQTTListener implements MqttCallbackExtended {
 	@Override
 	public void connectionLost(Throwable cause) {
 		System.out.println("The MQTT Connection was lost! - will auto-reconnect");
-    }
+	}
 
 	@SpecAssertion(
-    		section = Sections.TOPICS_NAMESPACE_ELEMENT,
-    		id = "topic-structure-namespace-a")
+			section = Sections.PAYLOADS_B_PAYLOAD,
+			id = "payloads_timestamp_in_UTC")
+	@SpecAssertion(
+			section = Sections.TOPICS_NAMESPACE_ELEMENT,
+			id = "topic-structure-namespace-a")
 	@Override
 	public void messageArrived(String topic, MqttMessage message) throws Exception {
 		try {
-			if (topic.startsWith("STATE/") ) {
-				System.out.println("Sparkplug message: "+ topic + " " + new String(message.getPayload()));
+			if (topic.startsWith("STATE/")) {
+				System.out.println("Sparkplug message: " + topic + " " + new String(message.getPayload()));
 			} else if (topic.equals(log_topic_name)) {
-				System.out.println("TCK log: "+ new String(message.getPayload()));
+				System.out.println("TCK log: " + new String(message.getPayload()));
 			} else {
-
 				if (topic.startsWith("spAv1.0/")) {
 					log("Warning - non-standard Sparkplug A message received");
 					testResult("topic-structure-namespace-a", "FAIL");
 				} else {
+					System.out.println("Message arrived on Sparkplug topic " + topic);
 
-					Topic sparkplugTopic = TopicUtil.parseTopic(topic);
-					ObjectMapper mapper = new ObjectMapper();
-					mapper.setSerializationInclusion(Include.NON_NULL);
+					checkTopic(topic.split("/"));
 
-					System.out.println("Message arrived on Sparkplug topic " + sparkplugTopic.toString());
+					PayloadOrBuilder inboundPayload = Payload.parseFrom(message.getPayload());
+					System.out.println(inboundPayload.toString());
 
-					SparkplugBPayloadDecoder decoder = new SparkplugBPayloadDecoder();
-					SparkplugBPayload inboundPayload = decoder.buildFromByteArray(message.getPayload());
+					if (inboundPayload.hasTimestamp()) {
+						Date now = new Date();
+						long diff = now.getTime() - inboundPayload.getTimestamp();
 
-					// Convert the message to JSON and print to system.out
-					String payloadString = mapper.writeValueAsString(inboundPayload);
-					System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(inboundPayload));
+						if (diff < 0 || diff > 1000) {
+							testResult("payloads_timestamp_in_UTC", "FAIL");
+							System.out.println("Timestamp diff " + diff);
+						} else {
+							testResult("payloads_timestamp_in_UTC", "PASS");
+						}
+					}
 				}
 			}
 		} catch (Exception e) {
@@ -174,17 +187,17 @@ public class MQTTListener implements MqttCallbackExtended {
 	public void deliveryComplete(IMqttDeliveryToken token) {
 		System.out.println("Published message: " + token);
 	}
-	
+
 	private void testResult(String id, String state) {
 		// Don't override a failing test fail
 		if (!testResults.get(id).equals("FAIL")) {
 			testResults.put(id, state);
 		}
 	}
-	
+
 	/*@SpecAssertion(
-    		section = Sections.INTRODUCTION_SECONDARY_HOST_APPLICATION,
-    		id = "intro-secondary-host-state")
+			section = Sections.INTRODUCTION_SECONDARY_HOST_APPLICATION,
+			id = "intro-secondary-host-state")
 	public void checkState(String topic, MqttMessage message) {
 		String[] words = topic.split("/");
 		if (words.length == 2) {
@@ -196,43 +209,43 @@ public class MQTTListener implements MqttCallbackExtended {
 			log("Error: STATE message with wrong topic "+topic);
 		}
 	}*/
-	
-    @SpecAssertion(
-    		section = Sections.TOPICS_SPARKPLUG_TOPIC_NAMESPACE_ELEMENTS,
-    		id = "topic-structure")
-    @SpecAssertion(
-    		section = Sections.INTRODUCTION_SPARKPLUG_IDS,
-    		id = "intro-group-id-string")
-    @SpecAssertion(
-    		section = Sections.INTRODUCTION_SPARKPLUG_IDS,
-    		id = "intro-group-id-chars")
-    @SpecAssertion(
-    		section = Sections.INTRODUCTION_SPARKPLUG_IDS,
-    		id = "intro-edge-node-id-string")
-    @SpecAssertion(
-    		section = Sections.INTRODUCTION_SPARKPLUG_IDS,
-    		id = "intro-edge-node-id-chars")
-    @SpecAssertion(
-    		section = Sections.INTRODUCTION_SPARKPLUG_IDS,
-    		id = "intro-device-id-string")
-    @SpecAssertion(
-    		section = Sections.INTRODUCTION_SPARKPLUG_IDS,
-    		id = "intro-device-id-chars")
-    @SpecAssertion(
-    		section = Sections.TOPICS_GROUP_ID_ELEMENT,
-    		id = "topic-structure-namespace-valid-group-id") 
-    @SpecAssertion(
-    		section = Sections.TOPICS_EDGE_NODE_ID_ELEMENT,
-    		id = "topic-structure-namespace-valid-edge-node-id") 
-    @SpecAssertion(
-    		section = Sections.TOPICS_DEVICE_ID_ELEMENT,
-    		id = "topic-structure-namespace-valid-device-id") 
-    @SpecAssertion(
-    		section = Sections.TOPICS_DEVICE_ID_ELEMENT,
-    		id = "topic-structure-namespace-device-id-associated-message-types")
-    @SpecAssertion(
-    		section = Sections.TOPICS_DEVICE_ID_ELEMENT,
-    		id = "topic-structure-namespace-device-id-non-associated-message-types")
+
+	@SpecAssertion(
+			section = Sections.TOPICS_SPARKPLUG_TOPIC_NAMESPACE_ELEMENTS,
+			id = "topic-structure")
+	@SpecAssertion(
+			section = Sections.INTRODUCTION_SPARKPLUG_IDS,
+			id = "intro-group-id-string")
+	@SpecAssertion(
+			section = Sections.INTRODUCTION_SPARKPLUG_IDS,
+			id = "intro-group-id-chars")
+	@SpecAssertion(
+			section = Sections.INTRODUCTION_SPARKPLUG_IDS,
+			id = "intro-edge-node-id-string")
+	@SpecAssertion(
+			section = Sections.INTRODUCTION_SPARKPLUG_IDS,
+			id = "intro-edge-node-id-chars")
+	@SpecAssertion(
+			section = Sections.INTRODUCTION_SPARKPLUG_IDS,
+			id = "intro-device-id-string")
+	@SpecAssertion(
+			section = Sections.INTRODUCTION_SPARKPLUG_IDS,
+			id = "intro-device-id-chars")
+	@SpecAssertion(
+			section = Sections.TOPICS_GROUP_ID_ELEMENT,
+			id = "topic-structure-namespace-valid-group-id")
+	@SpecAssertion(
+			section = Sections.TOPICS_EDGE_NODE_ID_ELEMENT,
+			id = "topic-structure-namespace-valid-edge-node-id")
+	@SpecAssertion(
+			section = Sections.TOPICS_DEVICE_ID_ELEMENT,
+			id = "topic-structure-namespace-valid-device-id")
+	@SpecAssertion(
+			section = Sections.TOPICS_DEVICE_ID_ELEMENT,
+			id = "topic-structure-namespace-device-id-associated-message-types")
+	@SpecAssertion(
+			section = Sections.TOPICS_DEVICE_ID_ELEMENT,
+			id = "topic-structure-namespace-device-id-non-associated-message-types")
 	public void checkTopic(String[] elements) {
 		String result = "FAIL";
 		if (elements[0].equals("STATE")) {
@@ -252,24 +265,20 @@ public class MQTTListener implements MqttCallbackExtended {
 				if (elements.length >= 5) {
 					device_id = elements[4];
 				}
-				
-				if (message_type.equals("DBIRTH") || 
-					message_type.equals("DDEATH") ||
-					message_type.equals("DDATA") ||
-					message_type.equals("DCMD")) {
+
+				if (message_type.equals("DBIRTH") || message_type.equals("DDEATH") || message_type.equals("DDATA")
+						|| message_type.equals("DCMD")) {
 					if (elements.length != 5) {
-						result = "FAIL";	
+						result = "FAIL";
 						testResult("topic-structure-namespace-device-id-associated-message-types", "FAIL");
 					} else {
 						testResult("topic-structure-namespace-device-id-associated-message-types", "TRUE");
 					}
-				}  
-					
-				if (message_type.equals("NBIRTH") || 
-					message_type.equals("NDEATH") ||
-					message_type.equals("NDATA") ||
-					message_type.equals("NCMD")) {
-					if (elements.length != 4) { 
+				}
+
+				if (message_type.equals("NBIRTH") || message_type.equals("NDEATH") || message_type.equals("NDATA")
+						|| message_type.equals("NCMD")) {
+					if (elements.length != 4) {
 						result = "FAIL";
 						testResult("topic-structure-namespace-device-id-non-associated-message-types", "FAIL");
 					} else {
@@ -277,75 +286,74 @@ public class MQTTListener implements MqttCallbackExtended {
 					}
 				}
 				testResult("topic-structure", result);
-				
+
 				result = "TRUE";
 				if (!checkUTF8String(group_id)) {
 					result = "FAIL";
-					testResult("intro-group-id-string", "FAIL");	
+					testResult("intro-group-id-string", "FAIL");
 					log("Group id string is invalid");
 				}
-				
+
 				if (!checkMQTTChars(group_id)) {
 					result = "FAIL";
-					testResult("intro-group-id-chars", "FAIL");	
+					testResult("intro-group-id-chars", "FAIL");
 					log("Group id chars are invalid");
 				}
 				testResult("topic-structure-namespace-valid-group-id", result);
-				
+
 				result = "TRUE";
 				if (!checkUTF8String(edge_node_id)) {
 					result = "FAIL";
-					testResult("intro-edge-node-id-string", "FAIL");	
+					testResult("intro-edge-node-id-string", "FAIL");
 					log("Edge node id string is invalid");
 				}
-				
+
 				if (!checkMQTTChars(edge_node_id)) {
 					result = "FAIL";
-					testResult("intro-edge-node-id-chars", "FAIL");	
+					testResult("intro-edge-node-id-chars", "FAIL");
 					log("Edge node id chars are invalid");
 				}
 				testResult("topic-structure-namespace-valid-edge-node-id", result);
-				
-				result = "TRUE";
-				if (!checkUTF8String(device_id)) {
-					result = "FAIL";
-					testResult("intro-device-id-string", "FAIL");	
-					log("Device id string is invalid");
+
+				if (device_id != null) {
+					result = "TRUE";
+					if (!checkUTF8String(device_id)) {
+						result = "FAIL";
+						testResult("intro-device-id-string", "FAIL");
+						log("Device id string is invalid");
+					}
+
+					if (!checkMQTTChars(device_id)) {
+						result = "FAIL";
+						testResult("intro-device-id-chars", "FAIL");
+						log("Device id chars are invalid");
+					}
+					testResult("topic-structure-namespace-valid-device-id", result);
 				}
-				
-				if (!checkMQTTChars(device_id)) {
-					result = "FAIL";
-					testResult("intro-device-id-chars", "FAIL");	
-					log("Device id chars are invalid");
-				}
-				testResult("topic-structure-namespace-valid-device-id", result);
-				
 			}
 		}
 	}
-    
-    private boolean checkUTF8String(String inString) {
-    	// MUST be a valid UTF-8 string 
 
-    	byte[] bytes = inString.getBytes(StandardCharsets.UTF_8);
+	private boolean checkUTF8String(String inString) {
+		// MUST be a valid UTF-8 string
 
-    	String utf8EncodedString = new String(bytes, StandardCharsets.UTF_8);
+		byte[] bytes = inString.getBytes(StandardCharsets.UTF_8);
 
-    	boolean rc = false;
-    	if (inString.equals(utf8EncodedString)) {
-    		rc = true;
-    	}
-    	return rc;
-    }
-    
-    private boolean checkMQTTChars(String inString) {
-    	// MUST not use reserved characters of + (plus), / (forward slash), and # (number sign).
-    	boolean rc = true; 
-    	if ((inString.indexOf('+') != -1) ||
-    		(inString.indexOf('/') != -1) ||
-    		(inString.indexOf('#') != -1)) {
-    		rc = false;
-    	}
-    	return rc;
-    }
+		String utf8EncodedString = new String(bytes, StandardCharsets.UTF_8);
+
+		boolean rc = false;
+		if (inString.equals(utf8EncodedString)) {
+			rc = true;
+		}
+		return rc;
+	}
+
+	private boolean checkMQTTChars(String inString) {
+		// MUST not use reserved characters of + (plus), / (forward slash), and # (number sign).
+		boolean rc = true;
+		if ((inString.indexOf('+') != -1) || (inString.indexOf('/') != -1) || (inString.indexOf('#') != -1)) {
+			rc = false;
+		}
+		return rc;
+	}
 }
--- tck/src/main/java/org/eclipse/sparkplug/tck/test/TCKTest.java
+++ tck/src/main/java/org/eclipse/sparkplug/tck/test/TCKTest.java
@@ -1,5 +1,5 @@
 /* ******************************************************************************
- * Copyright (c) 2021 Ian Craggs
+ * Copyright (c) 2021, 2022 Ian Craggs
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
@@ -30,61 +30,45 @@ import org.slf4j.LoggerFactory;
 import java.nio.ByteBuffer;
 import java.util.Map;
 
+import org.eclipse.sparkplug.tck.test.common.Utils;
+import org.eclipse.sparkplug.tck.test.Monitor;
+
 /**
  * @author Ian Craggs
  * @author Lukas Brand
  */
 public abstract class TCKTest {
 
-    private static final @NotNull Logger logger = LoggerFactory.getLogger("Sparkplug");
+	private static final @NotNull Logger logger = LoggerFactory.getLogger("Sparkplug");
 
-    public abstract void connect(String clientId, ConnectPacket packet);
+	public abstract void connect(String clientId, ConnectPacket packet);
 
-    public abstract void disconnect(String clientId, DisconnectPacket packet);
+	public abstract void disconnect(String clientId, DisconnectPacket packet);
 
-    public abstract void subscribe(String clientId, SubscribePacket packet);
+	public abstract void subscribe(String clientId, SubscribePacket packet);
 
-    public abstract void publish(String clientId, PublishPacket packet);
+	public abstract void publish(String clientId, PublishPacket packet);
 
-    public abstract String getName();
+	public abstract String getName();
 
-    public abstract Map<String, String> getResults();
+	public abstract Map<String, String> getResults();
 
-    public abstract String[] getTestIds();
+	public abstract String[] getTestIds();
 
-    public abstract void endTest();
+	public abstract void endTest();
 
-    public void reportResults(final @NotNull Map<String, String> results) {
-        logger.info("Summary Test Results for {} ", getName());
-        final StringBuilder summary = new StringBuilder();
+	public void endTest(Map<String, String> results) {
 
-        String overall = results.entrySet().isEmpty() ? TopicConstants.EMPTY : TopicConstants.PASS;
+	}
 
-        for (final Map.Entry<String, String> reportResult : results.entrySet()) {
-            logger.info("\t {}:{} ", reportResult.getKey(), reportResult.getValue());
-            summary.append(reportResult.getKey())
-                    .append(": ")
-                    .append(reportResult.getValue())
-                    .append(";")
-                    .append(System.lineSeparator());
+	public void reportResults(final @NotNull Map<String, String> results) {
+		logger.info("Summary Test Results for {} ", getName());
 
-            if (!reportResult.getValue().startsWith(TopicConstants.PASS)) {
-                overall = TopicConstants.FAIL;
-            }
-        }
-        logger.info("OVERALL:{} ", overall);
+		final StringBuilder summary = Utils.getSummary(results);
 
-        summary.append("OVERALL: ")
-                .append(overall)
-                .append(";")
-                .append(System.lineSeparator());
-
-        final PublishService publishService = Services.publishService();
-        final Publish message = Builders.publish()
-                .topic("SPARKPLUG_TCK/RESULT")
-                .qos(Qos.AT_LEAST_ONCE)
-                .payload(ByteBuffer.wrap(summary.toString().getBytes()))
-                .build();
-        publishService.publish(message);
-    }
+		final PublishService publishService = Services.publishService();
+		final Publish message = Builders.publish().topic("SPARKPLUG_TCK/RESULT").qos(Qos.AT_LEAST_ONCE)
+				.payload(ByteBuffer.wrap(summary.toString().getBytes())).build();
+		publishService.publish(message);
+	}
 }
