f3acc064e38985dcdde6bab76391da24222eba7b
wes-johnson <wes0johnson@gmail.com>
2022-12-17 09:05:43 -0800

    More jar signing debugging

--- sign.sh
+++ sign.sh
@@ -2,4 +2,4 @@
 
 
 # Sign the TCK jar
-curl -vvvvs -o tck/build/hivemq-extension/sparkplug-tck-3.0.0-signed.jar -F "file=@tck/build/hivemq-extension/sparkplug-tck-3.0.0.jar" https://cbi.eclipse.org/jarsigner/sign
+curl -vvvvs -o tck/build/hivemq-extension/sparkplug-tck-3.0.0-signed.jar -F file=@tck/build/hivemq-extension/sparkplug-tck-3.0.0.jar "https://cbi.eclipse.org/jarsigner/sign"
